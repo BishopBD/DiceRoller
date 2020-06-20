2b7e075123b398ea41dcf01dd946b90d9dceac2c
Brian Bishop <briguy032@gmail.com>
2020-06-20 14:26:55 -0400

    comment out chat.js, register dice option onclick

--- razor/SignalR/wwwroot/js/dice.js
+++ razor/SignalR/wwwroot/js/dice.js
@@ -1,28 +1,21 @@
-﻿//connection.on("ReceiveMessage", function (user, message) {
-//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//    var encodedMsg = user + " says " + msg;
-//    var li = document.createElement("li");
-//    li.textContent = encodedMsg;
-//    document.getElementById("messagesList").appendChild(li);
-//});
-//var diceIds =
-//    [
-//        "d4",
-//        "d6",
-//        "d8",
-//        "d10",
-//        "d12",
-//        "d20"
-//    ];
+﻿"use strict";
 
-//diceIds.forEach
+var diceIds =
+    [
+        "d4",
+        "d6",
+        "d8",
+        "d10",
+        "d12",
+        "d20"
+    ];
 
-//function registerDiceOnClick(value) {
+diceIds.forEach(registerDiceOnClick);
 
-//}
-
-document.getElementById("d4").onclick = function () {
-    var li = document.createElement("li");
-    li.textContent = "D4";
-    document.getElementById("diceSelection").appendChild(li);
+function registerDiceOnClick(value) {
+    document.getElementById(value).onclick = function () {
+        var li = document.createElement("li");
+        li.textContent = value.toUpperCase();
+        document.getElementById("diceSelection").appendChild(li);
+    }
 }
