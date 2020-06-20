7f2f8687700e9801322b50fc4d21c95d50e908ab
Brian Bishop <briguy032@gmail.com>
2020-06-20 12:07:26 -0400

    delete bin/obj, add dice.js behavior for d4

--- razor/SignalR/wwwroot/js/chat.js
+++ razor/SignalR/wwwroot/js/chat.js
@@ -3,7 +3,7 @@
 var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 
 //Disable send button until connection is established
-document.getElementById("sendButton").disabled = true;
+//document.getElementById("sendButton").disabled = true;
 
 connection.on("ReceiveMessage", function (user, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -14,7 +14,7 @@ connection.on("ReceiveMessage", function (user, message) {
 });
 
 connection.start().then(function () {
-    document.getElementById("sendButton").disabled = false;
+    //document.getElementById("sendButton").disabled = false;
 }).catch(function (err) {
     return console.error(err.toString());
 });
---
+++ razor/SignalR/wwwroot/js/dice.js
@@ -0,0 +1,28 @@
+﻿//connection.on("ReceiveMessage", function (user, message) {
+//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//    var encodedMsg = user + " says " + msg;
+//    var li = document.createElement("li");
+//    li.textContent = encodedMsg;
+//    document.getElementById("messagesList").appendChild(li);
+//});
+//var diceIds =
+//    [
+//        "d4",
+//        "d6",
+//        "d8",
+//        "d10",
+//        "d12",
+//        "d20"
+//    ];
+
+//diceIds.forEach
+
+//function registerDiceOnClick(value) {
+
+//}
+
+document.getElementById("d4").onclick = function () {
+    var li = document.createElement("li");
+    li.textContent = "D4";
+    document.getElementById("diceSelection").appendChild(li);
+}
